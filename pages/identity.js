b105f60442070826ad721baf6ac7fbe920195bc0
gaalih <galih@mail.unpas.ac.id>
2022-06-30 16:48:57 +0700

    up

--- pages/identity.js
+++ pages/identity.js
@@ -22,6 +22,7 @@ function Formidentity() {
                       className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                       id="inline-full-name"
                       type="number"
+                      required
                     />
                   </div>
                 </div>
@@ -39,6 +40,7 @@ function Formidentity() {
                       className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                       id="inline-full-name"
                       type="text"
+                      required
                     />
                   </div>
                 </div>
@@ -54,35 +56,25 @@ function Formidentity() {
                     </label>
                   </div>
                   <div className="w-4/6">
-                    <div className="flex items-center">
-                      <div className="avatar">
-                        <div className="w-24 mask mask-squircle">
-                          <img src="https://placeimg.com/192/192/people" />
-                        </div>
+                    <div className="avatar">
+                      <div className="w-24 mask mask-squircle">
+                        <img src="https://placeimg.com/192/192/people" />
                       </div>
-                      <div className="mx-6">
-                        <button className="btn btn-circle btn-outline">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
-                            />
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
-                            />
-                          </svg>
-                        </button>
-                      </div>
+                    </div>
+                    <div className="my-3">
+                      <label className="block">
+                        <span className="sr-only">Choose File</span>
+                        <input
+                          type="file"
+                          className="text-sm text-grey-500
+                          file:mr-5 file:py-2 file:px-6
+                          file:rounded-full file:border-0
+                          file:text-sm file:font-medium
+                          file:bg-slate-300 file:text-grey-300
+                          hover:file:cursor-pointer hover:file:bg-slate-200
+                          hover:file:text-grey-700 "
+                        />
+                      </label>
                     </div>
                   </div>
                 </div>
@@ -92,7 +84,7 @@ function Formidentity() {
                   <div className="w-4/6">
                     <button
                       className="shadow w-2/4 bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                      type="button"
+                      type="submit"
                     >
                       Sign Up
                     </button>
