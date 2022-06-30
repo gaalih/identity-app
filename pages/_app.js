d0bd8db5fb21dbb5a63b4373fb5ba626a2556a0e
gaalih <galih@mail.unpas.ac.id>
2022-06-30 15:50:00 +0700

    tambahkan form identitas

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,55 @@
 import "../styles/global.css";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <nav className="navbar mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabindex="0" className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabindex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Homepage</a>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          {/* <p className="font-bold">Form Identity</p> */}
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Logo</a>
+        </div>
+      </nav>
+      <div className="container mx-auto mt-5">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- pages/identity.js
+++ pages/identity.js
@@ -1,7 +1,110 @@
 import React from "react";
 
 function Formidentity() {
-  return <div>Formidentity</div>;
+  return (
+    <div id="form-identity">
+      <div className="grid grid-cols-6 gap-4">
+        <div className="col-start-2 col-span-4">
+          <div className="card rounded card-side bg-base-100 shadow-xl shadow-slate-200">
+            <div className="card-body mx-auto">
+              <form action="" className="mx-auto w-3/5">
+                <div className="flex items-center mb-6">
+                  <div className="w-1/3">
+                    <label
+                      className="block text-gray-500 font-bold text-left mb-1 mb-0 pr-4"
+                      for="inline-full-name"
+                    >
+                      Phone Number
+                    </label>
+                  </div>
+                  <div className="w-4/6">
+                    <input
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      id="inline-full-name"
+                      type="number"
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center mb-6">
+                  <div className="w-1/3">
+                    <label
+                      className="block text-gray-500 font-bold text-left mb-1 mb-0 pr-4"
+                      for="inline-full-name"
+                    >
+                      ID Card Number
+                    </label>
+                  </div>
+                  <div className="w-4/6">
+                    <input
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      id="inline-full-name"
+                      type="text"
+                    />
+                  </div>
+                </div>
+                <hr />
+                <br />
+                <div className="flex items-center mb-6">
+                  <div className="w-1/3">
+                    <label
+                      className="block text-gray-500 font-bold text-left mb-1 mb-0 pr-4"
+                      for="inline-full-name"
+                    >
+                      Photo Selfie
+                    </label>
+                  </div>
+                  <div className="w-4/6">
+                    <div className="flex items-center">
+                      <div className="avatar">
+                        <div className="w-24 mask mask-squircle">
+                          <img src="https://placeimg.com/192/192/people" />
+                        </div>
+                      </div>
+                      <div className="mx-6">
+                        <button className="btn btn-circle btn-outline">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+                            />
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex items-center">
+                  <div className="w-1/3"></div>
+                  <div className="w-4/6">
+                    <button
+                      className="shadow w-2/4 bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                      type="button"
+                    >
+                      Sign Up
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Formidentity;
